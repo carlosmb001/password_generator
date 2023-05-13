850f08203368543d22f4bae10a880722b7fedbc2
Carlos Martinez Baltazar <42792565+carlosmb001@users.noreply.github.com>
2023-05-13 07:04:16 -0700

    added array characters

--- script.js
+++ script.js
@@ -2,6 +2,21 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+//total of 88 characters
+const characters = [
+  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+  "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
+  "u", "v", "w", "x", "y", "z", "A", "B", "C", "D",
+  "E", "F", "G", "H", "I", "J","K", "L", "M", "N", 
+  "O", "P", "Q", "R", "S", "T","U", "V", "W", "X",
+  "Y", "Z","!", "@", "#", "$", "%", "^", "&", "*",
+  "(", ")","-", "_", "+", "=", "|", "{", "}", "~",
+  ":", ";","<", ">", ",", ".", "/", "?"
+];
+
+console.log(characters.length);
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
