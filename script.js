aa2d7e735448a5a7047abdf2d8f57c8818252fa4
Carlos Martinez Baltazar <42792565+carlosmb001@users.noreply.github.com>
2023-05-18 23:48:51 -0700

    added password array

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-window.input;
 
 // Write password to the #password input
 
-const number = ("1234567890");    
-const lowerCase = ("abcdefghijklmnopqrstuvwxyz");
-const upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-const specialCharacter = ["!@#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"];
+function writePassword() {
+  
+  var input = prompt("Choose a length of at least 8 characters and no more than 128 characters:")
+  var numbers = prompt("Do you want to use numbers? (y/n):");
+  var lowerCase = prompt("Do you want to use lowercase letters? (y/n):");
+  var upperCase = prompt("Do you want to use uppercase letters? (y/n):");
+  var specialCharacters = prompt("Do you want to use special characters? (y/n):");
 
+  const characters = [];
 
-function askUser(){
-input = prompt("Choose a length of at least 8 characters and no more than 128 characters:")
-number = prompt("Do you want to use numbers? (y/n):");
-lowerCase = prompt("Do you want to use lowercase letters? (y/n):");
-upperCase = prompt("Do you want to use uppercase letters? (y/n):");
+  if (input >= 8 && input <= 128) {
+    if (numbers === "y") {
+    characters.push("1234567890");
+    }
+    if (lowerCase === "y") {
+    characters.push("abcdefghijklmnopqrstuvwxyz");
+    }
+    if (upperCase === "y") {
+    characters.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    }
+    if (specialCharacters === "y") {
+    characters.push("!@#$%&'()*+,-./:;<=>?@[\\]^_`{|}~");
+    }
+  } else {
+  alert("The length of the password must be between 8 and 128 characters.");
+  return;
 }
 
-console.log(characters.length);
-
-function writePassword() {
+}
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+console.log(writePassword);
