8b653602abeadd0c9a427579b7191a6518c02b46
Carlos Martinez Baltazar <42792565+carlosmb001@users.noreply.github.com>
2023-05-18 18:27:53 -0700

    separated array

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+window.input;
 
 // Write password to the #password input
 //total of 88 characters
-const characters = [
-  "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-  "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
-  "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
-  "u", "v", "w", "x", "y", "z", "A", "B", "C", "D",
-  "E", "F", "G", "H", "I", "J","K", "L", "M", "N", 
-  "O", "P", "Q", "R", "S", "T","U", "V", "W", "X",
-  "Y", "Z","!", "@", "#", "$", "%", "^", "&", "*",
-  "(", ")","-", "_", "+", "=", "|", "{", "}", "~",
-  ":", ";","<", ">", ",", ".", "/", "?"
-];
-console.log(characters.length); // check the array length
-
-let input = prompt("How long do you want the password to be?")
+const number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z"] 
+const upperCase = ["A", "B", "C", "D","E", "F", "G", "H", "I", "J","K", "L", "M", "N","O", "P", "Q", "R", "S", "T","U", "V", "W", "X","Y", "Z"]
+const specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*","(", ")","-", "_", "+", "=", "|", "{", "}", "~",":", ";","<", ">", ",", ".", "/", "?"];
 
 
+function askUser(){
+input = prompt("Choose a length of at least 8 characters and no more than 128 characters:")
+return input;
+}
 
 console.log(characters.length);
 
